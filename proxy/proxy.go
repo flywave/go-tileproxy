256313702a607dfd88fbd74c08b66e04d5d645bb
aninggo <aninggo@gmail.com>
2021-07-03 20:53:25 +0800

    update

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,17 +1,3 @@
-// Copyright 2018 Adam Tauber
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package proxy
 
 import (
@@ -37,11 +23,6 @@ func (r *roundRobinSwitcher) GetProxy(pr *http.Request) (*url.URL, error) {
 	return u, nil
 }
 
-// RoundRobinProxySwitcher creates a proxy switcher function which rotates
-// ProxyURLs on every request.
-// The proxy type is determined by the URL scheme. "http", "https"
-// and "socks5" are supported. If the scheme is empty,
-// "http" is assumed.
 func RoundRobinProxySwitcher(ProxyURLs ...string) (crawler.ProxyFunc, error) {
 	if len(ProxyURLs) < 1 {
 		return nil, crawler.ErrEmptyProxyURL

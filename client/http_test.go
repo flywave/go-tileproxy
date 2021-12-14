f7c4f06b954465e8e48b0055e1ede74424b72da8
aninggo <aninggo@gmail.com>
2021-12-14 13:31:37 +0800

    update

--- client/http_test.go
+++ client/http_test.go
@@ -1,9 +1,17 @@
 package client
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/flywave/go-tileproxy/utils"
 )
 
 var (
@@ -51,6 +59,61 @@ func TestHttpFetch(t *testing.T) {
 	}
 
 	wg.Wait()
+
+	if len(testResult) == 0 {
+		t.FailNow()
+	}
+}
+
+func Get(url string) []byte {
+
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	var buffer [512]byte
+	result := bytes.NewBuffer(nil)
+	for {
+		n, err := resp.Body.Read(buffer[0:])
+		result.Write(buffer[0:n])
+		if err != nil && err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+	}
+
+	return result.Bytes()
+}
+
+func get_url(client *CollectorClient, url string, font, name string, i int) {
+	if !utils.FileExists(fmt.Sprintf("%s/%s", font, name)) {
+		data := Get(url)
+
+		ioutil.WriteFile(fmt.Sprintf("%s/%s", font, name), data, os.ModePerm)
+	}
+}
+
+func TestFetchFonts(t *testing.T) {
+	client := NewCollectorClient(&httpConf, nil)
+
+	fonts := []string{
+		"DINOffcProRegularNotoSansCJKSCDemiLightArialUnicodeMSRegular",
+		"DINOffcProMediumNotoSansCJKSCMediumArialUnicodeMSRegular",
+		"Noto Sans CJK SC DemiLight,Arial Unicode MS Regular",
+		"NotoSansCJKSCDemiLightArialUnicodeMSRegular",
+		"NotoSansCJKSCMediumArialUnicodeMSRegular",
+		"NotoSansCJKSCMediumArialUnicodeMSBold",
+	}
+
+	for _, font := range fonts {
+		for i := 19968; i < 65535; i += 256 {
+			url := fmt.Sprintf("https://api.luokuang.com/emg/fonts/%s/%d-%d.pbf?ak=DB1589338764061116C2A51A2CF1B144F2BFA1D29334859EE996VY8OQZR7MGD8", font, i, i+255)
+			get_url(client, url, font, fmt.Sprintf("%d-%d.pbf", i, i+255), i)
+		}
+	}
 
 	if len(testResult) == 0 {
 		t.FailNow()
--- resource/mapbox_test.go
+++ resource/mapbox_test.go
@@ -1,1 +1,19 @@
 package resource
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestE(t *testing.T) {
+
+	f, _ := os.Open("./style.json")
+
+	data, _ := io.ReadAll(f)
+
+	style := ExtractStyle(data, "data.styleContent")
+
+	ioutil.WriteFile("./mbstyle.json", style.GetData(), os.ModePerm)
+}

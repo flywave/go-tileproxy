f23f8b244f079da0d1e4b521a917c1fb45ec967d
aninggo <aninggo@gmail.com>
2022-01-11 17:52:22 +0800

    update

--- terrain/geotiff_test.go
+++ terrain/geotiff_test.go
@@ -1,11 +1,103 @@
 package terrain
 
 import (
+	"fmt"
+	"image"
 	"os"
 	"testing"
 
+	"github.com/flywave/go-cog"
 	"github.com/flywave/go-geo"
+	"github.com/flywave/go-tileproxy/tile"
+
+	vec2d "github.com/flywave/go3d/float64/vec2"
 )
+
+func TestGetGeotiff(t *testing.T) {
+	bbox := vec2d.Rect{
+		Min: vec2d.T{117.820129394, 36.815331734},
+		Max: vec2d.T{117.882614135, 36.853801657},
+	}
+
+	srs900913 := geo.NewProj(900913)
+	srs4326 := geo.NewProj(4326)
+
+	conf := geo.DefaultTileGridOptions()
+	conf[geo.TILEGRID_SRS] = srs900913
+	conf[geo.TILEGRID_RES_FACTOR] = 2.0
+	conf[geo.TILEGRID_TILE_SIZE] = []uint32{512, 512}
+	conf[geo.TILEGRID_ORIGIN] = geo.ORIGIN_UL
+
+	grid := geo.NewTileGrid(conf)
+
+	r, l, _ := grid.GetAffectedBBoxAndLevel(bbox, [2]uint32{512, 512}, srs4326)
+
+	cbox, grids, it, err := grid.GetAffectedLevelTiles(r, l)
+
+	if err != nil {
+		t.FailNow()
+	}
+
+	tilesCoord := [][3]int{}
+	minx, miny := 0, 0
+	for {
+		x, y, z, done := it.Next()
+
+		if minx == 0 || x < minx {
+			minx = x
+		}
+
+		if miny == 0 || y < miny {
+			miny = y
+		}
+
+		tilesCoord = append(tilesCoord, [3]int{x, y, z})
+
+		if done {
+			break
+		}
+	}
+
+	if len(tilesCoord) == 0 {
+		t.FailNow()
+	}
+
+	var sources []tile.Source
+	opts := &RasterOptions{Format: tile.TileFormat("webp"), Mode: BORDER_BILATERAL}
+
+	for i := range tilesCoord {
+		z, x, y := tilesCoord[i][2], tilesCoord[i][0], tilesCoord[i][1]
+
+		source := NewDemRasterSource(ModeMapbox, opts)
+
+		b := grid.TileBBox([3]int{x, y, z}, false)
+
+		source.georef = geo.NewGeoReference(b, srs900913)
+
+		source.SetSource(fmt.Sprintf("../data/%d_%d_%d.webp", z, x, y))
+
+		sources = append(sources, source)
+	}
+
+	m := NewRasterMerger(grids, [2]uint32{512, 512})
+	rr := m.Merge(sources, opts)
+
+	tiledata := rr.GetTile().(*TileData)
+
+	io := &DemIO{Mode: ModeMapbox, Format: tile.TileFormat("webp")}
+
+	raw, _ := io.Encode(tiledata)
+
+	f, _ := os.Create("./zhoucun.webp")
+	f.Write(raw)
+	f.Close()
+
+	rect := image.Rect(0, 0, int(tiledata.Size[0]), int(tiledata.Size[1]))
+
+	src := cog.NewSource(tiledata.Datas, &rect, cog.CTLZW)
+
+	cog.WriteTile("./zhoucun.tif", src, cbox, srs900913, tiledata.Size, nil)
+}
 
 func TestGeoTIFF(t *testing.T) {
 	f, _ := os.Open("../data/14_13733_6366.webp")

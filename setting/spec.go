2fbf3e5824ea900dd20cc5f2a52f8f84ce479f0d
hj <hawking2215@gmail.com>
2022-05-19 18:28:38 +0800

    update

--- setting/spec.go
+++ setting/spec.go
@@ -22,13 +22,14 @@ const (
 type ServiceType string
 
 const (
-	NONE_SERVICE      ServiceType = "none"
-	WMS_SERVICE       ServiceType = "wms"
-	TMS_SERVICE       ServiceType = "tms"
-	WMTS_SERVICE      ServiceType = "wmts"
-	WMTS_REST_SERVICE ServiceType = "wmts_rest"
-	MAPBOX_SERVICE    ServiceType = "mapbox"
-	CESIUM_SERVICE    ServiceType = "cesium"
+	NONE_SERVICE           ServiceType = "none"
+	WMS_SERVICE            ServiceType = "wms"
+	TMS_SERVICE            ServiceType = "tms"
+	WMTS_SERVICE           ServiceType = "wmts"
+	WMTS_REST_SERVICE      ServiceType = "wmts_rest"
+	MAPBOX_VECTOR_SERVICE  ServiceType = "mapbox_vector"
+	MAPBOX_TERRAIN_SERVICE ServiceType = "mapbox_terrain"
+	CESIUM_SERVICE         ServiceType = "cesium"
 )
 
 type CacheType string
